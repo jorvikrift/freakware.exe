b8c29b9b260b5f0811cad9663af8f2a423d15d4f
Jorvik <xurelxurel100@gmail.com>
2024-09-01 22:49:54 -0400

    Update data.lua

--- util/data.lua
+++ util/data.lua
@@ -4,6 +4,4 @@ local Data = {
   ["Invite"] = "discord.gg/there isnt a fucking link yet",
 }
 
-local UiName = Data["Name"] .. " | " .. Data["Version"] or "freakware.exe | unknown"
-
-return Data, UiName
+return Data
